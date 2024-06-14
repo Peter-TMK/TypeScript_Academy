4e75529c767695f4b80149af2844ffda79da897b
Peter-TMK <tundepeter01@gmail.com>
2024-06-14 20:12:50 +0100

    new updates

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,9 +1,29 @@
 // import React from "react";
 
+const Header = () => {
+  return <h2>Fun facts about React</h2>;
+};
+
+const Body = () => {
+  return (
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Ws originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps including mobile apps.</li>
+    </ul>
+  );
+};
+
+const Logo = () => <img width="80px" src="../react_logo.png" />;
+
 const App = () => {
   return (
     <>
-      <h2>Hello React</h2>
+      <Logo />
+      <Header />
+      <Body />
     </>
   );
 };
